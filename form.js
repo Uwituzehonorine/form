9f19f99d0fafee599bc751042f1afa196bed42a8
unknown <honorineuwituze896@gmail.com>
2022-10-19 07:34:51 +0200

    modified

--- form.js
+++ form.js
@@ -1,7 +1,10 @@
-function myFunction() {
-    let x = document.forms["myForm"]["name"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  }
+function myFunction(){
+    var username = document.getElementById("username");
+    var email=document.getElementById("email");
+    var field1= document.getElementById("field1");
+    var field2= document.getElementById("field2");
+    field1.innerHTML="Please enter your name";
+    field2.innerHTML="Please enter your Email";
+    document.getElementsByClassName(".input-control input").style.color= "red";
+    var x= document.querySelectorAll(".in")
+}
